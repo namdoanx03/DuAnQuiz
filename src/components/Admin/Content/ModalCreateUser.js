47fb03dafa7541911896a1b4ef15a7ca1d99c2b9
namdoanx03 <dpnbro1102@gmail.com>
2024-11-06 22:23:46 +0700

    58

---
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -0,0 +1,119 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { SlPlus } from "react-icons/sl";
+import axios from 'axios';
+
+const ModalCreateUser = (props) => {
+    const { show, setShow } = props; //lay dc prop tu cha tryen xuong, prop la 1 bien obj
+
+    const handleClose = () => {
+        setShow(false)
+        setEmail("");
+        setPassword("")
+        setUsername("")
+        setRole("")
+        setImage("")
+        setPreviewImage("")
+    };
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [username, setUsername] = useState("")
+    const [role, setRole] = useState("USER")
+    const [image, setImage] = useState("")
+
+    const handleUploadImage = (event) =>{
+        if(event.target && event.target.files && event.target.files[0]){
+            setPreviewImage(URL.createObjectURL(event.target.files[0])) //URL.createObjectURL: tao 1 duong link URl tai localhost duoi dang blod, phia client co the doc va hien thi duoc
+            setImage(event.target.file[0])
+        }else{
+            // setPreviewImage("")
+        }
+    }
+    const handleSubmitCreateUsr = async() => {
+        //validate
+
+        //call api
+        // let data = {
+        //     email: email,
+        //     password: password,
+        //     username: username,
+        //     role: role,
+        //     userImage:image
+        // }
+        // console.log(data)
+        const FormData = require('form-data');
+
+        const data = new FormData();
+        data.append('email', email);
+        data.append('password', password);
+        data.append('username', username);
+        data.append('role', role);
+        data.append('userImage', data);
+
+        let res = await axios.post('http://localhost:8081/api/v1/participant', data)
+        console.log("check ___-", res)
+    }
+    const [previewImage, setPreviewImage] = useState("")
+
+    return (
+        <>
+            {/* <Button variant="primary" onClick={handleShow}>
+                Launch demo modal
+            </Button> */}
+
+            <Modal show={show} onHide={handleClose} size="xl" backdrop="static" className='modal-add-user'>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add new user</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form className="row g-3">
+                        <div className="col-md-6">
+                            <label for="inputEmail4" className="form-label">Email</label>
+                            <input type="email" className="form-control" id="inputEmail4" value={email} onChange={(event) => setEmail(event.target.value)}/>
+                        </div>
+                        <div className="col-md-6">
+                            <label for="inputPassword4" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="inputPassword4" value={password} onChange={(event) => setPassword(event.target.value)} />
+                        </div>
+                        <div className="col-md-6">
+                            <label for="inputCity" className="form-label">Username</label>
+                            <input type="text" className="form-control" id="inputCity" value={username} onChange={(event) => setUsername(event.target.value)} />
+                        </div>
+                        <div className="col-md-4">
+                            <label for="inputState" className="form-label">Role</label>
+                            <select id="inputState" className="form-select" onChange={(event) => setRole(event.target.value)}>
+                                <option value="USER">USER</option>
+                                <option value="ADMIN">ADMIN</option>
+                            </select>
+                        </div>
+                        <div className='"col-md-12'>
+                            <label className="form-label label-upload" htmlFor="labelUpload">
+                                <SlPlus /> Upload File Image
+                            </label>
+                            <input type='file' id='labelUpload' hidden  onChange={(event) => handleUploadImage(event) }/>
+                        </div>
+                        <div className='col-md-12 img-preview'>
+                            {previewImage ? 
+                                <img src={previewImage} />
+                                :
+                                <span>Preview Image</span>
+                            }    
+                        </div>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={() => handleSubmitCreateUsr()}>
+                        Save
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default ModalCreateUser
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import User from './components/User/User';
 import Admin from './components/Admin/Admin';
 import HomePage from './components/Home/HomePage';
+import Dashboard from './components/Admin/Content/DashBoard';
+import ManageUser from './components/Admin/Content/ManageUser';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
@@ -19,7 +21,11 @@ root.render(
           <Route path="/" element={<App />}>
             <Route index element={<HomePage />} />
             <Route path="users" element={<User />} />
-            <Route path="admins" element={<Admin />} />
+          </Route>
+
+          <Route path="/admins" element={<Admin />}>
+            <Route index element={<Dashboard />} />
+            <Route path="manage-users" element={<ManageUser />} />
           </Route>
         </Routes>
       </BrowserRouter>
