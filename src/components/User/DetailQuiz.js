1677a043cd58a4993f5fb1cbcb9ba388e7a04e8a
namdoanx03 <dpnbro1102@gmail.com>
2024-11-12 05:33:29 +0700

    process data

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom"; // khai bao mot tham so tren duong link URL
 import { getDataQuiz } from "../services/apiService";
-
+import _ from 'lodash';
 
 const DetailQuiz = (props) => {
     const params = useParams()
@@ -14,6 +14,28 @@ const DetailQuiz = (props) => {
     const fetchQuestion = async() => {
         let res = await getDataQuiz(quizId)
         console.log("check question" , res)
+        if(res && res.EC === 0){
+            let raw = res.DT
+            let data = _.chain(raw)
+                // Group the elements of Array based on `color` property
+                .groupBy("id") // gop tat ca cac object theo mot nhom ma chung ta quy dinh 
+                // `key` is group's name (color), `value` is the array of objects
+                .map((value, key) => {
+                    let answers = []
+                    let questionDescription, image = null
+
+                    value.forEach((item, index) => { // index lap tu 0 den phan tu cuoi cua mang
+                        if(index === 0) { //lay 1 phan tu vi cac phan tu o sau data giong het
+                            questionDescription = item.description
+                            image = item.image
+                        }
+                        answers.push(item.answers)
+                    })
+                    return { questionId: key, answers, questionDescription, image }
+                })
+                .value()
+            console.log("check", data)
+        }
     }
     console.log("check paream", params)
     return (
